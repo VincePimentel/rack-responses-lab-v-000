8e895318090608a4296d6e6d502756bfc32551bc
Vince Andrew Pimentel <contact@vincep.me>
2018-12-27 02:31:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,15 @@
-require 'pry'
-
 class Application
   def call(env)
     resp = Rack::Response.new
 
     time = Time.now
 
-    resp.finish
+    if time.hour <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
 
-    binding.pry
+    resp.finish
   end
 end
